04d6984c0b6eecdc660e71e1da3612ea8d2fcc37
Johdasgran <dampig14@gmail.com>
2022-06-08 20:05:39 -0500

    update app

--- script.js
+++ script.js
@@ -8,23 +8,20 @@ let vpHeight = window.innerHeight;
 console.log(documentHeight)
 console.log(vpHeight)
 
-scrollTo(0, documentHeight-vpHeight)
 console.log(window.scrollTo(0, documentHeight-vpHeight))
 
-var last_known_scroll_position = 0;
 var ticking = false;
 console.log(scrollTo)
 
-window.addEventListener("scroll", () => {
+window.addEventListener("scrollY", () => {
 
-    last_known_scroll_position = window.scrollY;
-    if (!ticking) {
-      window.requestAnimationFrame(function() {
-        doSomething(last_known_scroll_position);
-        ticking = false;
-      });
-    }
-    ticking = true;
+    // scrollTo(0,vpHeight - documentHeight)
+    scrollTo(vpHeight - documentHeight)
+
+
+
+
+
 })
 
 
