e72701458dc568cbd362c20181285aaa71a01885
Johdasgran <dampig14@gmail.com>
2022-06-08 20:00:27 -0500

    scroll smmoth

--- script.js
+++ script.js
@@ -5,6 +5,26 @@ console.log(documentHeight)
 let vpHeight = window.innerHeight;
 
 
-
+console.log(documentHeight)
+console.log(vpHeight)
 
 scrollTo(0, documentHeight-vpHeight)
+console.log(window.scrollTo(0, documentHeight-vpHeight))
+
+var last_known_scroll_position = 0;
+var ticking = false;
+console.log(scrollTo)
+
+window.addEventListener("scroll", () => {
+
+    last_known_scroll_position = window.scrollY;
+    if (!ticking) {
+      window.requestAnimationFrame(function() {
+        doSomething(last_known_scroll_position);
+        ticking = false;
+      });
+    }
+    ticking = true;
+})
+
+
