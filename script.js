4bca5e2cf62ab6f1790828b6a8c28366d68679f5
Johdasgran <dampig14@gmail.com>
2022-06-11 18:25:54 -0500

    looking for inspirtation

--- script.js
+++ script.js
@@ -27,3 +27,24 @@ window.addEventListener("scrollY", () => {
 // https://dribbble.com/shots/16429598-Music-player
 
 // https://dribbble.com/shots/16741029-Music-Streaming-Web-App/attachments/11786938?mode=media
+
+// https://dribbble.com/shots/17644369-Web-Music-Player/attachments/12798434?mode=media
+
+//  https://dribbble.com/shots/15593083/attachments/7379986?mode=media
+
+// https://dribbble.com/shots/15184731-Daft-Punk/attachments/6926976?mode=media
+
+// https://dribbble.com/shots/16894605-Music-Player-Web-App/attachments/11959653?mode=media
+
+// https://dribbble.com/shots/17409554-Spotify-App-Redesign-Concept
+
+
+
+
+
+
+
+
+
+
+// https://dribbble.com/shots/17067630-Acoustic-Mobile-Design-for-Music-Storeg
