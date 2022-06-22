3e9a04cccea6e112ec6b902db53efc62f934557d
Johdasgran <dampig14@gmail.com>
2022-06-21 22:29:15 -0500

    add video.js

--- script.js
+++ script.js
@@ -17,8 +17,8 @@
 //       console.log(body);
 //   });
 
+import videoPlayer from "./video.js";
+import audioPlayer from "./audio.js";
 
-import audioPlayer from './audio.js'
-
-
+videoPlayer()
 audioPlayer()
