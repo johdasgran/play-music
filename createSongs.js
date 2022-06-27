1ca9eb7a5fd090bf71ff1d05e48090ba7e4f5431
Johdasgran <dampig14@gmail.com>
2022-06-26 23:38:27 -0500

    add scroll bar

--- createSongs.js
+++ createSongs.js
@@ -1,7 +1,29 @@
+export const createSongs = fetch("./API.json")
+  .then((resp) => resp.json())
+  .then((data) => console.log(data));
 
 
-export const createSongs = fetch('./API.json')
-.then(resp => resp.json())
-.then(data => console.log(data));
 
 
+function createSong(params) {
+
+
+  let container = document.querySelector("div#app");
+  
+  // Container image
+  let card_image = document.createElement("div");
+  card_image.id = "image";
+  card_image.classList = "wobble-ver-right";
+  container.appendChild(card_image);
+
+  // Image
+  let image = document.createElement("img");
+  image.src = img_url;
+  image.id = id;
+  image.classList = `card-image ${id}`;
+  card_image.appendChild(image);
+
+
+
+
+}
